8649c7a249faad4c15c03d47263680407ed898a1
Danilo Shiroma <danilo.shiroma@gmail.com>
2022-02-21 23:25:02 -0400

    refactor: altera projeto para typescript

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,6 +1,12 @@
-const repositoryName = "unform"
+interface RepositoryItemProps {
+  repository: {
+    name: string,
+    description: string,
+    html_url: string
+  }
+}
 
-export function RepositoryItem(props) {
+export function RepositoryItem(props: RepositoryItemProps) {
   return (
     <li>
       <strong>{props.repository.name || "Sem nome"}</strong>
